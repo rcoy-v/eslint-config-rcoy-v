e28f329c37ce7c8c471ccb39411f0ebcd2c4dc92
rcoy-v <rcoy.vance@gmail.com>
2016-12-03 02:31:29 +0000

    Add rules for eslint mocha plugin

---
+++ rules/mocha.js
@@ -0,0 +1,21 @@
+module.exports = {
+  "rules": {
+    "mocha/handle-done-callback": 2,
+    "mocha/max-top-level-suites": [2, {"limit": 1}],
+    "mocha/no-exclusive-tests": 2,
+    "mocha/no-global-tests": 2,
+    "mocha/no-hooks": 0,
+    "mocha/no-hooks-for-single-case": 0,
+    "mocha/no-identical-title": 2,
+    "mocha/no-mocha-arrows": 0,
+    "mocha/no-nested-tests": 2,
+    "mocha/no-pending-tests": 2,
+    "mocha/no-return-and-callback": 2,
+    "mocha/no-sibling-hooks": 2,
+    "mocha/no-skipped-tests": 2,
+    "mocha/no-synchronous-tests": 0,
+    "mocha/no-top-level-hooks": 2,
+    "mocha/valid-suite-description": 0,
+    "mocha/valid-test-description": 2
+  }
+};
