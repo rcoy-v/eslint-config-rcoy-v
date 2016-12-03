93c1b589209a41d35210bd77eddbfe8f8165712e
rcoy-v <rcoy.vance@gmail.com>
2016-12-03 02:30:00 +0000

    Add initial eslint rules and license

---
+++ rules/es6.js
@@ -0,0 +1,29 @@
+module.exports = {
+  "rules": {
+    "arrow-body-style": 0,
+    "arrow-parens": [2, "always"],
+    "arrow-spacing": 2,
+    "constructor-super": 2,
+    "generator-star-spacing": 2,
+    "no-class-assign": 2,
+    "no-confusing-arrow": 2,
+    "no-constant-condition": 2,
+    "no-const-assign": 2,
+    "no-dupe-class-members": 2,
+    "no-duplicate-imports": [2, {"includeExports": true}],
+    "no-this-before-super": 2,
+    "no-useless-computed-key": 2,
+    "no-useless-rename": 2,
+    "no-var": 2,
+    "object-shorthand": 0,
+    "prefer-arrow-callback": 0,
+    "prefer-const": 2,
+    "prefer-numeric-literals": 0,
+    "prefer-reflect": 0,
+    "prefer-spread": 0,
+    "prefer-template": 2,
+    "require-yield": 2,
+    "rest-spread-spacing": [2, "never"],
+    "symbol-description": 2
+  }
+};
---
+++ rules/node.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "rules": {
+    "callback-return": 2,
+    "global-require": 0,
+    "handle-callback-error": 0,
+    "no-mixed-require": 0,
+    "no-new-require": 2,
+    "no-path-concat": 2,
+    "no-process-exit": 2,
+    "no-restricted-modules": 0,
+    "no-sync": 2
+  }
+};
